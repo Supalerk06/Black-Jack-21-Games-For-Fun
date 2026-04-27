ef187b1377c2efaee2541669fea90d922c8a545e
NatthasithBoonheng <127108005+NatthasithBoonheng@users.noreply.github.com>
2026-04-27 23:13:51 +0700

    main layout and basic view component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup>
+import { ref } from 'vue'
+import LobbyView from './components/LobbyView.vue'
+import GameView from './components/GameView.vue'
+import ResultView from './components/ResultView.vue'
 
+const gameState = ref('LOBBY')
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-
+  <div class="min-h-screen flex flex-col text-yellow-100 bg-[radial-gradient(ellipse_at_50%_30%,#2a1800_0%,#0d0900_70%,#050300_100%)]">
+    </div>
 </template>
+
+<style scoped>
+</style>
